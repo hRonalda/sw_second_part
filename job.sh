131de54cd99ef757364b7636a0fb2293357b7f9e
Qiao Zhaochen <51873940+JoeChen322@users.noreply.github.com>
2025-05-22 14:58:58 +0200

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,20 +1,50 @@
 #!/bin/bash
-#SBATCH --job-name=grayscale       # task name
-#SBATCH --output=output_%j.log               # outputfile
-#SBATCH --error=error_%j.log                 # error log
-#SBATCH --nodes=1                            # node number
-#SBATCH --ntasks=1                           # task number
-#SBATCH --cpus-per-task=4                    
-#SBATCH --mem=8G                             # mem for each node
-#SBATCH --time=01:00:00                      # max runnign time
-#SBATCH --partition=g100_all_serial      # Partition to submit job                
+
+# Set job name
+#SBATCH --job-name=grayscale_job       # Job name
+
+# Redirect output and error
+#SBATCH --output=grayscale.txt  # Output file
+#SBATCH --error=grayscale.txt    # Error file
+
+# Set Parameters
+#SBATCH --time=0:10:00                   # Time Limit (hh:mm:ss)   
+#SBATCH --ntasks=2                       # Number of tasks
+#SBATCH --nodes=1                        # Number of nodes
+#SBATCH --partition=g100_all_serial      # Partition to submit job
 
 # Module Load for Singularity
 module load singularity
 
-# set variables
-SIF_IMAGE="matrix_mult.sif"
-APP_COMMAND="/app/matrix_mult --input data.in --output result.out"
+# --------------------------------------------------------------------------------#
+# This line is necessary to prevent "No Protocol specified" warning from appearing.
+# --------------------------------------------------------------------------------#    
+export HWLOC_COMPONENTS=-gl
 
-# run Singularity container
-singularity exec $SIF_IMAGE $APP_COMMAND
+# Set TMPDIR to a writable directory (have done mkdir ~/tmp in advance, no need to create) 
+export TMPDIR=~/tmp
+
+# Set Open MPI environment variables to use the TMPDIR, which is a writable directory.
+export OMPI_MCA_tmpdir_base=$TMPDIR
+export OMPI_MCA_orte_tmpdir_base=$TMPDIR
+export OMPI_MCA_plm_rsh_agent="ssh :rsh"
+
+# Set MPI to use TCP as the communication protocol
+export OMPI_MCA_btl=self,tcp
+
+# Ensure Singularity TMP and Cache directories are set to writable locations
+export SINGULARITY_TMPDIR=$TMPDIR/singularity_tmp
+export SINGULARITY_CACHEDIR=$TMPDIR/singularity_cache
+
+# -----------------------------------------------------------------------------
+# Compare two ways to run a singularity container.
+# -----------------------------------------------------------------------------
+
+# Run application outside singularity container (Just for fun)
+srun singularity exec  ~/seproject/graysclae.sif mpiexec -np 2 ~/seproject/main
+
+# -----------------------------------------------------------------------------
+# Core Work Of This Job File: Do matrix multiplication by using MPI (2 tasks).
+# Run application inside singularity container (which is demaned by the project)
+# -----------------------------------------------------------------------------
+singularity exec ~/seproject/grayscale.sif mpirun -np 2 /opt/QiaoLibriciLiHuang_DevOps_second_part/main
